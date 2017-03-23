36f1afe922feff685024e44778256c9a31a84bb0
yarimorales <12470423@itcampeche.edu.mx>
2017-03-23 01:07:28 -0600

    Versión inicial del proyecto GeoApp

---
+++ app/src/main/java/com/jorgechuc/android/geoapp/GeoActivity.java
@@ -0,0 +1,13 @@
+package com.jorgechuc.android.geoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geo);
+    }
+}
